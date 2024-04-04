6bbdd28f27d74f1e6f737f2bbdc8809dfb82f196
Медведев Иван <ranzar2000@gmail.com>
2024-04-04 20:19:18 +0300

    Add StatsCardPropsand StatsCardsProps interfaces

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,19 @@
+import { GetFormStats } from "@/actions/form";
+import { ReactNode } from "react";
+
+export interface StatsCardProps {
+    title: string,
+    icon: ReactNode,
+    helperText: string,
+    value: string,
+    loading: boolean,
+    className: string
+}
+
+export interface StatsCardsProps {
+    data?: Awaited<ReturnType<typeof GetFormStats>>;
+    loading: boolean;
+}
 
 export interface FormStats {
     visits: number;
