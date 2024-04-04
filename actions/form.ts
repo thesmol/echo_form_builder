e5535b8b5a03094890e7566a4184f780eea9b78c
Медведев Иван <ranzar2000@gmail.com>
2024-04-04 17:18:19 +0300

    Add getFormStats function for user form statistics
    The `getFormStats` function in the `form.ts` file retrieves and calculates statistics for forms created by the current user, including the total number of visits, submissions, submission rate, and bounce rate. It utilizes Prisma for database operations and Clerk for user authentication.
    
    Changes include:
    - Importing Prisma and Clerk's `currentUser` function.
    - Defining a custom `UserNotFoundError` class.
    - Implementing the `getFormStats` function with JSDoc comments for clarity.

---
+++ actions/form.ts
@@ -0,0 +1,53 @@
+"use server"
+
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs";
+
+class UserNotFoundError extends Error { }
+
+/**
+ * Retrieves and calculates statistics for forms created by the current user.
+ * 
+ * This function fetches the total number of visits and submissions for all forms
+ * created by the current user. It then calculates the submission rate and bounce rate
+ * based on these statistics.
+ * 
+ * @returns {Promise<{visits: number, submissions: number, submissionRate: number, bounceRate: number}>}
+ * An object containing the total number of visits, submissions, submission rate, and bounce rate.
+ * 
+ * @throws {UserNotFoundError} If the current user cannot be determined (e.g., not logged in).
+ */
+export async function getFormStats() {
+    const user = await currentUser();
+    if (!user) {
+        throw new UserNotFoundError();
+    }
+
+    const stats = await prisma.form.aggregate({
+        where: {
+            userId: user.id
+        },
+        _sum: {
+            visits: true,
+            submissions: true
+        }
+    })
+
+    const visits = stats._sum.visits || 0;
+    const submissions = stats._sum.submissions || 0;
+
+    let submissionRate = 0;
+
+    if (visits > 0) {
+        submissionRate = (submissions / visits) * 100;
+    }
+
+    const bounceRate = 100 - submissionRate;
+
+    return {
+        visits,
+        submissions,
+        submissionRate,
+        bounceRate
+    }
+}
