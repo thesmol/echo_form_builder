3cb86370c202c3105d0c2c937a43a78fef148662
Медведев Иван <ranzar2000@gmail.com>
2024-04-04 20:21:00 +0300

    Implement StatsCard component for displaying statistics
    The StatsCard component, designed to display form statistics in a visually appealing and informative manner. The component accepts various props, including title, icon, helper text, value, loading state, and custom class names, to ensure flexibility and customization. It utilizes the Card, CardContent, CardHeader, and CardTitle components from the UI library for structuring the card, and the Skeleton component to display a loading state.
    
    Key changes include:
    - Importing necessary components and types from the UI library and types directory.
    - Defining the StatsCard functional component that renders a card with a header containing the title and icon, and a content area displaying the value and helper text.
    - Implementing conditional rendering to display a Skeleton component when the data is loading, and the actual value once the data is loaded.
    - Exporting the StatsCard component for use in other parts of the application.

---
+++ components/StatsCard.tsx
@@ -0,0 +1,23 @@
+import { StatsCardProps } from "@/types/types";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Skeleton } from "./ui/skeleton";
+
+function StatsCard({ title, icon, helperText, value, loading, className }: StatsCardProps) {
+    return (
+        <Card className={className}>
+            <CardHeader className="flex flex-row items-center justify-between pb-2">
+                <CardTitle className="text-sm font-medium text-muted-foreground">{title}</CardTitle>
+                {icon}
+            </CardHeader>
+            <CardContent>
+                <div className="text-2xl font-bold">
+                    {loading && <Skeleton><span className="opacity-0">0</span></Skeleton>}
+                    {!loading && value}
+                </div>
+                <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default StatsCard;
