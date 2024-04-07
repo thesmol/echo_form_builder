410a6f28aeb435895ac430059171e6ed2ee5e73d
Медведев Иван <ranzar2000@gmail.com>
2024-04-07 17:48:50 +0300

    Enhance CreationForm with name and description fields

---
+++ components/CreationForm.tsx
@@ -0,0 +1,51 @@
+import { ICreationFormProps } from "@/types/types";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "./ui/form";
+import { Input } from './ui/input';
+import { Textarea } from './ui/textarea';
+ 
+const CreationForm: React.FC<ICreationFormProps> = ({ form, onSubmit }) => {
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className='space-y-2'
+            >
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Название</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Описание</FormLabel>
+                            <FormControl>
+                                <Textarea rows={5} {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </form>
+        </Form>
+    );
+}
+
+export default CreationForm;
