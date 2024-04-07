85bbd7277f227e29d974b184f03735606c5da5e0
Медведев Иван <ranzar2000@gmail.com>
2024-04-07 18:08:07 +0300

    Add the CreatFormBtn component to the app layout

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import CardStatsWrapper, { StatsCards } from "@/components/CardStatsWrapper";
 import { Separator } from "@/components/ui/separator";
+import CreateFormBtn from "@/components/CreateFormBtn";
 
 export default function Home() {
   return (
@@ -11,7 +12,12 @@ export default function Home() {
         <CardStatsWrapper />
       </Suspense>
 
-      <Separator className="my-6"/>
+      <Separator className="my-6" />
+      <h2 className="text-4xl font-bold col-span-2">Ваши формы</h2>
+      <Separator className="my-6" />
+      <div className="grid frid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <CreateFormBtn />
+      </div>
     </div>
   );
 }
