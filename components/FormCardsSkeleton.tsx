23121948ab3406e1676525dacedda9b633fd3078
Медведев Иван <ranzar2000@gmail.com>
2024-04-10 15:44:00 +0300

    Add FormCardsSkeleton component
    the FormCardsSkeleton component displays a skeleton screen, providing a placeholder while the forms data is being loaded.

---
+++ components/FormCardsSkeleton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Skeleton } from './ui/skeleton'
+
+function FormCardsSkeleton() {
+    return (
+        <Skeleton className='border-2 border-primary-/20 h-[190px] w-full' />
+    )
+}
+
+export default FormCardsSkeleton
