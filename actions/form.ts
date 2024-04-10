c19fc9846dee168c561bf8ed2cb6d8736593e204
Медведев Иван <ranzar2000@gmail.com>
2024-04-10 14:31:26 +0300

    Extract user logic to getCurrentUser function
    - Moved the logic for retrieving the current user from the authentication context into a separate function named `getCurrentUser`.
    - Updated JSDoc comments in `GetFormStats` and `CreateForm` functions to reflect the use of `getCurrentUser`.
    - This change improves code modularity and readability by centralizing user retrieval logic.

--- actions/form.ts
+++ actions/form.ts
@@ -8,23 +8,39 @@ import { currentUser } from "@clerk/nextjs";
 class UserNotFoundError extends Error { }
 
 /**
+ * Retrieves the current user from the authentication context.
+ * 
+ * This function is used to fetch the current user's information from the authentication context.
+ * It throws a `UserNotFoundError` if the user is not found or not authenticated.
+ * 
+ * @returns {Promise<IUser>} A promise that resolves to the current user's information.
+ * @throws {UserNotFoundError} If the current user is not found or not authenticated.
+ */
+export async function getCurrentUser(): Promise<IUser> {
+    const user: IUser | null = await currentUser();
+    if (!user) {
+        throw new UserNotFoundError();
+    }
+    return user;
+}
+
+/**
  * Retrieves and calculates statistics for forms created by the current user.
  * 
  * This function fetches the total number of visits and submissions for all forms
  * created by the current user. It then calculates the submission rate and bounce rate
  * based on these statistics.
+ * 
  * @async
  * @function
  * @returns {Promise<{visits, submissions, submissionRate, bounceRate}>}
  * An object containing the total number of visits, submissions, submission rate, and bounce rate.
- *
+ * 
  * @throws {UserNotFoundError} If the current user cannot be determined (e.g., not logged in).
+ * @throws {Error} If an error occurs during the retrieval of form statistics.
  */
 export async function GetFormStats(): Promise<IFormStats> {
-    const user: IUser | null = await currentUser();
-    if (!user) {
-        throw new UserNotFoundError();
-    }
+    const user: IUser = await getCurrentUser();
 
     const stats = await prisma.form.aggregate({
         where: {
@@ -62,6 +78,7 @@ export async function GetFormStats(): Promise<IFormStats> {
  * @function
  * @param {formSchemaType} data - The form data, including name and description.
  * @returns {Promise<number>} The ID of the created form.
+ * 
  * @throws {Error} If the form is filled out incorrectly.
  * @throws {UserNotFoundError} If the current user is not found.
  * @throws {Error} If an error occurs during form creation.
@@ -72,10 +89,7 @@ export async function CreateForm(data: formSchemaType): Promise<number> {
         throw new Error("Форма не прошла валидацию, данные заполнены некорректно");
     }
 
-    const user = await currentUser();
-    if (!user) {
-        throw new UserNotFoundError();
-    }
+    const user: IUser = await getCurrentUser();
 
     const { name, description } = data;
 
