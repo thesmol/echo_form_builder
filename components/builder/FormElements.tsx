878a9ea1d582cff626cd758fd85a42b41eec57d7
Медведев Иван <ranzar2000@gmail.com>
2024-04-12 16:48:31 +0300

    Add FormElements mapping
    `FormElements` is a mapping from `ElementsType` to `FormElement`, which allows to easily access the correct `FormElement` based on the `ElementsType`. Currently, it includes a mapping for "TextField" to `TextFieldFormElement`.

---
+++ components/builder/FormElements.tsx
@@ -0,0 +1,7 @@
+import { FormElementsType } from '@/types/types'
+import React from 'react'
+import { TextFieldFormElement } from '../fields/TextField'
+
+export const FormElements: FormElementsType = {
+    TextField: TextFieldFormElement
+}
