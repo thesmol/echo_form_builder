62c7fbddffb071792a101c3c7cbc04e082a6bfa9
Медведев Иван <ranzar2000@gmail.com>
2024-04-12 16:43:53 +0300

    Add form elements types and components
    - `ElementsType`: This type is used to define the different types of form elements that form builder can handle. Currently, it only includes "TextField".
    
    - `FormElement`: This type describes the structure of a form element, including its type, a constructor function, button element for the designer interface, and the components used in the designer, form, and properties interfaces.
    
    - `FormElementIstance`: This type represents an instance of a form element, including its id, type, and any extra attributes.
    
    - `FormElementsType`: This type is a mapping from `ElementsType` to `FormElement`, allowing us to easily access the correct `FormElement` based on the `ElementsType`.

--- types/types.ts
+++ types/types.ts
@@ -32,3 +32,30 @@ export interface ICreationFormProps {
     form: UseFormReturn<formSchemaType>;
     onSubmit: (values: formSchemaType) => Promise<void>;
 }
+
+export type ElementsType = "TextField";
+
+export type FormElement = {
+    type: ElementsType;
+
+    construct: (id: string) => FormElementIstance;
+
+    designerBtnElement: {
+        icon: React.ElementType;
+        label: string;
+    }
+
+    designerComponent: React.FC;
+    formComponent: React.FC;
+    propertiesComponent: React.FC;
+};
+
+export type FormElementIstance = {
+    id: string;
+    type: ElementsType;
+    extraAttributes?: Record<string, any>;
+}
+
+export type FormElementsType = {
+    [key in ElementsType]: FormElement
+}
