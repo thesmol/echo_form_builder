3a8616b1088d8026c86cb4220081247a084d2875
Медведев Иван <ranzar2000@gmail.com>
2024-04-13 23:40:47 +0300

    Update DragOverlayWrapper Component
    The update allows the component to display the dragged element’s representation during the drag operation. If the dragged item is a sidebar button element, it displays the SidebarBtnElementDragOverlay. If the dragged item is a designer element, it finds the corresponding element and displays its designer component.

--- components/builder/DragOverlayWrapper.tsx
+++ components/builder/DragOverlayWrapper.tsx
@@ -3,8 +3,10 @@ import React, { useState } from 'react'
 import { SidebarBtnElementDragOverlay } from './SidebarBtnElement';
 import { FormElements } from './FormElements';
 import { ElementsType } from '@/types/types';
+import useDesigner from '@/hooks/useDesigner';
 
 function DragOverlayWrapper() {
+    const { elements } = useDesigner();
     const [draggedItem, setDraggedItem] = useState<Active | null>(null);
 
     useDndMonitor({
@@ -22,11 +24,27 @@ function DragOverlayWrapper() {
     if (!draggedItem) return null;
 
     let node = <div>no drag overlay</div>
-    const isSidebarBtnElemet = draggedItem?.data?.current?.isDesignerBtnElement;
 
+    const isSidebarBtnElemet = draggedItem.data?.current?.isDesignerBtnElement;
     if (isSidebarBtnElemet) {
-        const type = draggedItem?.data?.current?.type as ElementsType;
+        const type = draggedItem.data?.current?.type as ElementsType;
         node = <SidebarBtnElementDragOverlay formElement={FormElements[type]} />
+    }
+
+    const idDesignerElement = draggedItem.data?.current?.isDesignerElement;
+    if (idDesignerElement) {
+        const elementId = draggedItem.data?.current?.elementId;
+        const element = elements.find(el => el.id === elementId);
+
+        if (!element) node = <div>Element not found</div>;
+        else {
+            const DesignerElementComponent = FormElements[element.type].designerComponent;
+            node = (
+                <div className='flex w-full h-[120px] items-center rounded-md bg-accent px-4 py-2 pointer-events-none opacity-80'>
+                    <DesignerElementComponent elementInstance={element} />
+                </div>
+            )
+        }
     }
 
     return (
