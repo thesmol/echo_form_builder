d825ab8cf86359eea86abb95e36cc0d5a43cf935
Медведев Иван <ranzar2000@gmail.com>
2024-04-13 23:38:45 +0300

    Update TextField Component in TextField.tsx
    In the TextField.tsx file, the TextFieldFormElement component was updated. New attributes for the form element were added, including label, helperText, required, and placeholder. Additionally, new components DesignerComponent, FormComponent, and PropertiesComponent were added, which represent different states of the form element

--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -1,9 +1,22 @@
 "use client"
 
-import { ElementsType, FormElement } from "@/types/types"
+import { ElementsType, FormElement, FormElementInstance } from "@/types/types"
+import { Label } from "@radix-ui/react-label";
 import { MdTextFields } from "react-icons/md";
+import { Input } from "../ui/input";
 
 const type: ElementsType = "TextField";
+
+const extraAttributes = {
+    label: "Текстовое поле",
+    helperText: "Заполните текстовое поле",
+    required: false,
+    placeholder: "Текстовое значение тут..."
+}
+
+export type CustomInstance = FormElementInstance & {
+    extraAttributes: typeof extraAttributes;
+}
 
 export const TextFieldFormElement: FormElement = {
     type,
@@ -11,19 +24,56 @@ export const TextFieldFormElement: FormElement = {
     construct: (id: string) => ({
         id,
         type,
-        extraAttributes: {
-            label: "Текстовое поле",
-            helperText: "Подсказка",
-            required: false,
-            placeHolder: "Значение тут"
-        }
+        extraAttributes
     }),
 
     designerBtnElement: {
         icon: MdTextFields,
         label: "Текстовое поле"
     },
-    designerComponent: () => <div>designerComponent</div>,
-    formComponent: () => <div>formComponent</div>,
-    propertiesComponent: () => <div>propertiesComponent</div>,
+    designerComponent: DesignerComponent,
+    formComponent: FormComponent,
+    propertiesComponent: PropertiesComponent,
+}
+
+function DesignerComponent({ elementInstance }: {
+    elementInstance: FormElementInstance
+}) {
+    const element = elementInstance as CustomInstance;
+    const { label, required, placeholder, helperText } = element.extraAttributes;
+
+    return (
+        <div className="flex flex-col gap-2 w-full">
+            <Label>
+                {label}
+                {required && "*"}
+            </Label>
+            <Input
+                readOnly
+                disabled
+                placeholder={placeholder}
+            />
+            {helperText && (
+                <p className="text-muted-foreground text-[0.8rem]">
+                    {helperText}
+                </p>
+            )}
+        </div>
+    )
+}
+
+function FormComponent() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+function PropertiesComponent() {
+    return (
+        <div>
+
+        </div>
+    )
 }
