a9c94b7497038486fd914c2a6191ada4178eaa82
Медведев Иван <ranzar2000@gmail.com>
2024-04-14 00:11:09 +0300

    Integrate DesignerElementWrapper in Designer component
    The Designer component has been updated to use the useDndMonitor hook from @dnd-kit/core to track the end of a drag event. If the dragged item is a designer button element, a new element is constructed and added to the form.
    
    The DesignerElementWrapper component has been integrated into the Designer component. This allows each element in the form to be wrapped in a DesignerElementWrapper, providing additional functionality for each element.
    
    Additionally, the useDesigner hook has been used to provide the elements and addElement functions, which are used to manage the elements in the form.

--- components/builder/Designer.tsx
+++ components/builder/Designer.tsx
@@ -2,16 +2,42 @@
 
 import React from 'react'
 import DesignerSidebar from './DesignerSidebar'
-import { useDroppable } from '@dnd-kit/core'
+import { DragEndEvent, useDndMonitor, useDroppable } from '@dnd-kit/core'
 import { cn } from '@/lib/utils'
+import useDesigner from '@/hooks/useDesigner'
+import { FormElements } from './FormElements'
+import { ElementsType } from '@/types/types'
+import { idGenerator } from '@/lib/idGenerator'
+import DesignerElementWrapper from './DesignerElementWrapper'
 
 function Designer() {
+    const { elements, addElement } = useDesigner();
+
     const droppable = useDroppable({
         id: "designer-drop-area",
         data: {
             idDesignerDropArea: true,
         }
     })
+
+    useDndMonitor({
+        onDragEnd: (event: DragEndEvent) => {
+            const { active, over } = event;
+            if (!active || !over) return;
+
+            const isDesignerBtnElement = active.data?.current?.isDesignerBtnElement;
+
+            if (isDesignerBtnElement) {
+                const type = active.data?.current?.type;
+                const newElement = FormElements[type as ElementsType].construct(
+                    idGenerator()
+                )
+
+                addElement(0, newElement);
+            }
+
+        },
+    });
 
     return (
         <div className='flex w-full h-full'>
@@ -22,14 +48,24 @@ function Designer() {
                         droppable.isOver && "ring-2 ring-primary/20"
                     )}
                 >
-                    {!droppable.isOver && (
+                    {!droppable.isOver && elements.length === 0 && (
                         <p className='text-3xl text-muted-foreground flex flex-grow items-center font-bold'>
                             Перетащите сюда
                         </p>
                     )}
-                    {droppable.isOver && (
+                    {droppable.isOver && elements.length === 0 && (
                         <div className="p-4 w-full">
                             <div className="h-[120px] rounded-md bg-primary/20"></div>
+                        </div>
+                    )}
+                    {elements.length > 0 && (
+                        <div className='flex flex-col w-full gap-2 p-4'>
+                            {elements.map(element => (
+                                <DesignerElementWrapper
+                                    key={element.id}
+                                    element={element}
+                                />
+                            ))}
                         </div>
                     )}
                 </div>
