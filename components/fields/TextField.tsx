c28554359a826bfd81436d6fc07580d23adf1b7f
Медведев Ваня <ranzar2000@gmail.com>
2024-05-31 20:04:32 +0300

    Update TextField element
    Property validation using zod schema has been added.
    PropertiesFormSchemaType typing was added
    PropertiesComponent that displays and handles extraAttributes fields of a TextField has been implemented

--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -4,6 +4,13 @@ import { ElementsType, FormElement, FormElementInstance } from "@/types/types"
 import { Label } from "@radix-ui/react-label";
 import { MdTextFields } from "react-icons/md";
 import { Input } from "../ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import useDesigner from "@/hooks/useDesigner";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Switch } from "../ui/switch";
 
 const type: ElementsType = "TextField";
 
@@ -14,9 +21,18 @@ const extraAttributes = {
     placeholder: "Текстовое значение тут..."
 }
 
-export type CustomInstance = FormElementInstance & {
+const propertiesSchema = z.object({
+    label: z.string().min(2, "Поле должно содержать как минимум 2 символа").max(80, "Поле может содержать максимум 80 символов"),
+    helperText: z.string().max(300, "Поле может содержать максимум 300 символов"),
+    required: z.boolean().default(false),
+    placeholder: z.string().max(80, "Поле может содержать максимум 80 символов"),
+})
+
+type CustomInstance = FormElementInstance & {
     extraAttributes: typeof extraAttributes;
 }
+
+type propertiesFormSchemaType = z.infer<typeof propertiesSchema>;
 
 export const TextFieldFormElement: FormElement = {
     type,
@@ -70,10 +86,138 @@ function FormComponent() {
     )
 }
 
-function PropertiesComponent() {
-    return (
-        <div>
+function PropertiesComponent({ elementInstance }: {
+    elementInstance: FormElementInstance
+}) {
+    const element = elementInstance as CustomInstance;
+    const { updateElement } = useDesigner();
 
-        </div>
+    const { label, required, placeholder, helperText } = element.extraAttributes;
+    const form = useForm<propertiesFormSchemaType>({
+        resolver: zodResolver(propertiesSchema),
+        mode: "onBlur",
+        defaultValues: {
+            label: label,
+            helperText: helperText,
+            placeholder: placeholder,
+            required: required,
+        }
+    });
+
+    useEffect(() => {
+        form.reset(element.extraAttributes);
+    }, [element, form]);
+
+    function applyChanges(values: propertiesFormSchemaType) {
+        const { label, required, placeholder, helperText } = values;
+        updateElement(element.id, {
+            ...element,
+            extraAttributes: {
+                label,
+                helperText,
+                placeholder,
+                required,
+            }
+        })
+    }
+
+    return (
+        <Form {...form}>
+            <form
+                onBlur={form.handleSubmit(applyChanges)}
+                className="space-y-3"
+                onSubmit={e => { e.preventDefault() }}
+            >
+                <FormField
+                    control={form.control}
+                    name="label"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>
+                                Заголовок
+                            </FormLabel>
+                            <FormControl>
+                                <Input {...field}
+                                    onKeyDown={e => {
+                                        if (e.key === "Enter") e.currentTarget.blur();
+                                    }}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                Заголовок поля, <br /> который будет отображен сверху
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="placeholder"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>
+                                Замещающий текст
+                            </FormLabel>
+                            <FormControl>
+                                <Input {...field}
+                                    onKeyDown={e => {
+                                        if (e.key === "Enter") e.currentTarget.blur();
+                                    }}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                Текст, <br /> который будет отображен внутри поля
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="helperText"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>
+                                Подсказка
+                            </FormLabel>
+                            <FormControl>
+                                <Input {...field}
+                                    onKeyDown={e => {
+                                        if (e.key === "Enter") e.currentTarget.blur();
+                                    }}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                Подсказка поля, <br /> которое будет отображено ниже поля
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="required"
+                    render={({ field }) => (
+                        <FormItem className="flex items-center justify-between rounded-lg border p-3 shadow-sm">
+                            <div className="space-y-0.5">
+                                <FormLabel>
+                                    Обязательное поле
+                                </FormLabel>
+                                <FormDescription>
+                                    Определение того <br /> является ли поле обязательным для заполнения
+                                </FormDescription>
+                            </div>
+                            <FormControl className="ml-3">
+                                <Switch
+                                    checked={field.value}
+                                    onCheckedChange={field.onChange}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </form>
+        </Form>
     )
 }
