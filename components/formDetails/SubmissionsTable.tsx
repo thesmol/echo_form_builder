2bc2f079dedfd6b46a473e5c51280fffc20449ed
Медведев Ваня <ranzar2000@gmail.com>
2024-06-02 19:08:40 +0300

    add SubmissionsTable component to display form submissions
    This commit adds a new component called `SubmissionsTable` located in components/formDetails/SubmissionsTable.tsx. This component is responsible for rendering a table with the submissions for a given form.
    
    The component takes the following prop:
    - `id`: The unique identifier of the form for which to display submissions.
    
    It first fetches the form data with its submissions using the `GetFormWithSubmissions` function. If the form is found, it parses the form content to determine the columns to display in the table. For each submission, it extracts the submission content and the submitted date.
    
    The component then renders a table with the following structure:
    - Table header: Displays the column labels (e.g., 'Name', 'Email', etc.) and a 'Submitted At' column.
    - Table body: Iterates over the submissions and renders a row for each submission, displaying the submitted values and the formatted submission date using the `formatDistance` function from the `date-fns` library.

---
+++ components/formDetails/SubmissionsTable.tsx
@@ -0,0 +1,96 @@
+import { GetFormWithSubmissions } from '@/actions/form';
+import { Column, ElementsType, FormElementInstance, Row } from '@/types/types';
+import React, { ReactNode } from 'react'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../ui/table';
+import { formatDistance } from 'date-fns';
+
+async function SubmissionsTable({ id }: { id: number }) {
+    const form = await GetFormWithSubmissions(id);
+
+    if (!form) {
+        throw new Error("form not found");
+    }
+
+    const formElements = JSON.parse(form.content) as FormElementInstance[];
+    const columns: Column[] = [];
+
+    formElements.forEach(element => {
+        switch (element.type) {
+            case "TextField":
+                columns.push({
+                    id: element.id,
+                    label: element.extraAttributes?.label,
+                    required: element.extraAttributes?.required,
+                    type: element.type
+                });
+                break;
+            default:
+                break;
+        }
+    })
+
+    const rows: Row[] = [];
+    form.FormSubmissions.forEach((submission) => {
+        const content = JSON.parse(submission.content);
+        rows.push({
+            ...content,
+            submittedAt: submission.createdAt
+        })
+    })
+
+    return (
+        <>
+            <h1 className="text-2xl font-bold my-4">Ответы</h1>
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableHead key={column.id} className='uppercase'>
+                                    {column.label}
+                                </TableHead>
+                            ))}
+                            <TableHead className='text-muted-forefround text-right uppercase'>
+                                Заполнено в
+                            </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {rows.map((row, index) => (
+                            <TableRow key={index}>
+                                {columns.map((column) => (
+                                    <RowCell
+                                        key={column.id}
+                                        type={column.type}
+                                        value={row[column.id]}
+                                    />
+                                ))}
+                                <TableCell className='text-muted-foreground text-right'>
+                                    {formatDistance(row.submittedAt, new Date(), {
+                                        addSuffix: true,
+                                    })}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+        </>
+    )
+}
+
+export default SubmissionsTable;
+
+function RowCell({
+    type,
+    value }: {
+        type: ElementsType,
+        value: string
+    }) {
+    let node: ReactNode = value;
+    return (
+        <TableCell>
+            {node}
+        </TableCell>
+    )
+}
