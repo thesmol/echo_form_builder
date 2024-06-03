b74c35b4ad47c13086ded0a5acbc5851c9b3bc0f
Медведев Ваня <ranzar2000@gmail.com>
2024-06-04 01:20:39 +0300

    Set max height styles for sidebar

--- components/builder/DesignerSidebar.tsx
+++ components/builder/DesignerSidebar.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { FormElements } from './FormElements';
-import SidebarBtnElement from './SidebarBtnElement';
 import useDesigner from '@/hooks/useDesigner';
 import FormElementsSidebar from './FormElementsSidebar';
 import PropertiesFormSidebar from './PropertiesFormSidebar';
@@ -9,7 +7,7 @@ function DesignerSidebar() {
     const { selectedElement } = useDesigner();
 
     return (
-        <aside className='w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-full'>
+        <aside className='w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto min-h-full h-fit'>
             {!selectedElement && <FormElementsSidebar />}
             {selectedElement && <PropertiesFormSidebar />}
         </aside>
